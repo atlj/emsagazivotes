f3abdb6294a0083ac02eb3a5214b170df73f9bee
atlj <cccswagboyyolo@yahoo.com>
2021-05-07 15:00:58 +0300

    feat: add indicator component

---
+++ src/components/indicator/index.tsx
@@ -0,0 +1,56 @@
+import cn from "classnames";
+import styles from "./index.module.css";
+
+interface Props {
+    index: number;
+    count: number;
+    className?: string;
+    gridclassName?: string;
+}
+
+const IndicatorItem = ({ itemIndex, currentIndex }) => {
+    return (
+        <li
+            className={cn(styles.indicatorItem, {
+                [styles.indicatorItemSelected]: itemIndex === currentIndex,
+            })}
+        ></li>
+    );
+};
+
+export const Indicator = ({
+    index,
+    count,
+    className,
+    gridclassName,
+}: Props) => {
+    return (
+        <div className={cn("flex", className)}>
+            <ul className={cn(styles.indicatorGrid, gridclassName)}>
+                {[...Array(count)].map((data, itemIndex) => (
+                    <IndicatorItem
+                        key={itemIndex}
+                        itemIndex={itemIndex}
+                        currentIndex={index}
+                    />
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+interface option {
+    name: string;
+    icon: string;
+}
+
+interface page {
+    title: string;
+    options: Array<option>;
+}
+
+interface data {
+    name: string;
+    icon: string;
+    pages: Array<page>;
+}
