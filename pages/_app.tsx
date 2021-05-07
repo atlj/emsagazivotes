d637d1f45401852a19a98df06de724a50dbb973c
atlj <cccswagboyyolo@yahoo.com>
2021-05-07 14:57:10 +0300

    feat: add work sans font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,21 @@ import { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 import "@styles/global.css";
 import { appWithTranslation } from "@i18n";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default appWithTranslation(MyApp);
